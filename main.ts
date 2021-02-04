87ceb062c021210c1ce6599153c2b11bfd9db52c
TecnoLògics Bellvitge <tecnologics@ibellvitge.net>
2021-02-04 07:49:55 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,8 @@
 function calcula () {
     voltageDiference = voltage - 0.4
     concentracion = voltageDiference * 50 / 16
-    serial.writeNumber(concentracion)
-    serial.writeLine(" ppm")
 }
-function decideix () {
+function decide () {
     if (voltage == 0) {
         serial.writeLine("problema")
     } else if (voltage < 400) {
@@ -13,14 +11,16 @@ function decideix () {
         calcula()
     }
 }
-let adcVal = 0
 let concentracion = 0
-let voltage = 0
 let voltageDiference = 0
-serial.writeLine("HI")
+let voltage = 0
+let adcVal = 0
+voltage = 0
+voltageDiference = 0
+concentracion = 0
 basic.forever(function () {
     adcVal = pins.analogReadPin(AnalogPin.P1)
     voltage = adcVal * (3300 / 1024)
-    decideix()
+    decide()
     basic.pause(1000)
 })
